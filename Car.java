dabf5a1a7549d0f74ae146a20de7875c2f8db31e
Aishwariya-R <114977837+Aishwariya-R@users.noreply.github.com>
2022-10-04 13:55:42 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+public class Car
+{
+	final String brand ="MaruthiSuzuki";
+	String model;
+	String varient;
+	int price;
+	public static void main(String[] args) 
+	{
+		Car c1 =new Car();
+		c1.model="swift";
+		c1.varient="VXI";
+		c1.price=713304;
+		System.out.println(c1.model+"of varient"+c1.varient"+"price is"+s1.price););
+		Car c2=new Car();
+		c2.model="ciaz";
+		c2.varient="zxi"; 
+		c2.price=1183989;
+		System.out.println(c2.model+"of varient"+c2.varient+"price is"+c2.price);
+		
+	}
+}
